5b069aa2ec828f38343c0026238eee9384b5900f
zhijxu-MS <zhijxu@microsoft.com>
2018-10-27 10:32:15 +0800

    grublock cell supported

--- tests/test_grublock.py
+++ tests/test_grublock.py
@@ -7,17 +7,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import unittest
-
-from termcolor import cprint
-
 import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import rnn
-from tensorflow.python.ops import init_ops
 from tensorflow.python.ops import variable_scope
-from tensorflow.contrib.rnn.python.ops import gru_ops
 from backend_test_base import Tf2OnnxBackendTestBase
+
 
 # pylint: disable=missing-docstring,invalid-name,unused-argument,using-constant-test
 
@@ -29,7 +24,7 @@ def save_graph(output_names_with_port):
         variables_lib.global_variables_initializer().run()
         # expected = sess.run(output_dict, feed_dict=feed_dict)
         output_name_without_port = [n.split(':')[0] for n in output_names_with_port]
-        #graph_def = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def,
+        # graph_def = tf.graph_util.convert_variables_to_constants(sess, sess.graph_def,
         #                                                         output_name_without_port)
 
         tf.train.write_graph(sess.graph_def, logdir=r'C:\Users\zhijxu\Desktop\GRU', name="ori.pb", as_text=True)
@@ -52,7 +47,6 @@ def save_tensorboard_with_graph():
     train_writer = tf.summary.FileWriter(logdir)
     train_writer.add_graph(sess.graph)
     train_writer.close()
-    cprint("save graph in tensorboard done", "green")
 
 
 class GRUBlockTests(Tf2OnnxBackendTestBase):
@@ -64,9 +58,9 @@ class GRUBlockTests(Tf2OnnxBackendTestBase):
 
         x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
 
-        with tf.name_scope("xzj"):
-            cell = gru_ops.GRUBlockCell(units)
-
+        # no scope
+        cell = rnn.GRUBlockCell(
+            units)
         outputs, cell_state = tf.nn.dynamic_rnn(
             cell,
             x,
@@ -78,7 +72,198 @@ class GRUBlockTests(Tf2OnnxBackendTestBase):
         input_names_with_port = ["input_1:0"]
         feed_dict = {"input_1:0": x_val}
         output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, rtol=1e-5)
+
+    def test_multiple_dynamic_gru(self):
+        units = 5
+        batch_size = 6
+        x_val = np.array([[1., 1.], [2., 2.], [3., 3.], [4., 4.]], dtype=np.float32)
+        x_val = np.stack([x_val] * batch_size)
+
+        x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
+        _ = tf.placeholder(tf.float32, x_val.shape, name="input_2")
+
+        gru_output_list = []
+        gru_cell_state_list = []
+        if True:
+            # no scope
+            cell = rnn.GRUBlockCell(
+                units)
+            outputs, cell_state = tf.nn.dynamic_rnn(
+                cell,
+                x,
+                dtype=tf.float32)
+            gru_output_list.append(outputs)
+            gru_cell_state_list.append(cell_state)
+
+        if True:
+            # given scope
+            cell = rnn.GRUBlockCell(
+                units)
+            with variable_scope.variable_scope("root1") as scope:
+                outputs, cell_state = tf.nn.dynamic_rnn(
+                    cell,
+                    x,
+                    dtype=tf.float32,
+                    sequence_length=[4, 4, 4, 4, 4, 4],
+                    scope=scope)
+            gru_output_list.append(outputs)
+            gru_cell_state_list.append(cell_state)
+
+        _ = tf.identity(gru_output_list, name="output")
+        _ = tf.identity(gru_cell_state_list, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val}
+        input_names_with_port = ["input_1:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, rtol=1e-5)
+
+    def test_single_dynamic_gru_seq_length_is_const(self):
+        units = 5
+        batch_size = 6
+        x_val = np.array([[1., 1.], [2., 2.], [3., 3.], [4., 4.], [5., 5.]], dtype=np.float32)
+        x_val = np.stack([x_val] * batch_size)
+        x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
+
+        # no scope
+        cell = rnn.GRUBlockCell(
+            units)
+        outputs, cell_state = tf.nn.dynamic_rnn(
+            cell,
+            x,
+            dtype=tf.float32,
+            sequence_length=[4, 3, 4, 5, 2, 1])
+
+        _ = tf.identity(outputs, name="output")
+        _ = tf.identity(cell_state, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val}
+        input_names_with_port = ["input_1:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, rtol=1e-4)
+
+    def test_single_dynamic_gru_seq_length_is_not_const(self):
+        units = 5
+        batch_size = 6
+        x_val = np.array([[1., 1.], [2., 2.], [3., 3.], [4., 4.], [5., 5.]], dtype=np.float32)
+        x_val = np.stack([x_val] * batch_size)
+        x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
+
+        y_val = np.array([4, 3, 4, 5, 2, 1], dtype=np.int32)
+        seq_length = tf.placeholder(tf.int32, y_val.shape, name="input_2")
+
+        # no scope
+        cell = rnn.GRUBlockCell(
+            units)
+        outputs, cell_state = tf.nn.dynamic_rnn(
+            cell,
+            x,
+            dtype=tf.float32,
+            sequence_length=tf.identity(seq_length))
+
+        _ = tf.identity(outputs, name="output")
+        _ = tf.identity(cell_state, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val, "input_2:0": y_val}
+        input_names_with_port = ["input_1:0", "input_2:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, rtol=1e-04)
+
+    def test_single_dynamic_gru_placeholder_input(self):
+        units = 5
+        x_val = np.array([[1., 1.], [2., 2.], [3., 3.], [4., 4.]], dtype=np.float32)
+        x_val = np.stack([x_val] * 6)
+        x = tf.placeholder(tf.float32, shape=(None, 4, 2), name="input_1")
+
+        # no scope
+        cell = rnn.GRUBlockCell(
+            units)
+        outputs, cell_state = tf.nn.dynamic_rnn(
+            cell,
+            x,
+            dtype=tf.float32)  # by default zero initializer is used
+
+        _ = tf.identity(outputs, name="output")
+        _ = tf.identity(cell_state, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val}
+        input_names_with_port = ["input_1:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, rtol=1e-5)
+
+    def test_single_dynamic_gru_ch_zero_state_initializer(self):
+        units = 5
+        batch_size = 6
+        x_val = np.array([[1., 1.], [2., 2.], [3., 3.], [4., 4.], [5., 5.]], dtype=np.float32)
+        x_val = np.stack([x_val] * batch_size)
+        x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
+        # no scope
+        cell = rnn.GRUBlockCell(
+            units)
+
+        # defining initial state
+        initial_state = cell.zero_state(batch_size, dtype=tf.float32)
+        outputs, cell_state = tf.nn.dynamic_rnn(
+            cell,
+            x,
+            initial_state=initial_state,
+            dtype=tf.float32)
+
+        _ = tf.identity(outputs, name="output")
+        _ = tf.identity(cell_state, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val}
+        input_names_with_port = ["input_1:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
         self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, rtol=1e-05)
+
+    def test_single_dynamic_gru_random_weights(self):
+        hidden_size = 5
+        batch_size = 6
+        x_val = np.array([[1., 1.], [2., 2.], [3., 3.], [4., 4.]], dtype=np.float32)
+        x_val = np.stack([x_val] * batch_size)
+
+        x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
+        # no scope
+        cell = rnn.GRUBlockCell(
+            hidden_size)
+
+        outputs, cell_state = tf.nn.dynamic_rnn(
+            cell,
+            x,
+            dtype=tf.float32)
+
+        _ = tf.identity(outputs, name="output")
+        _ = tf.identity(cell_state, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val}
+        input_names_with_port = ["input_1:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, 0.0001)
+
+    def test_single_dynamic_gru_random_weights2(self):
+        hidden_size = 128
+        batch_size = 1
+        x_val = np.random.randn(1, 133).astype('f')
+        x_val = np.stack([x_val] * batch_size)
+
+        x = tf.placeholder(tf.float32, x_val.shape, name="input_1")
+        # no scope
+        cell = rnn.GRUBlockCell(
+            hidden_size)
+
+        outputs, cell_state = tf.nn.dynamic_rnn(
+            cell,
+            x,
+            dtype=tf.float32)
+
+        _ = tf.identity(outputs, name="output")
+        _ = tf.identity(cell_state, name="cell_state")
+
+        feed_dict = {"input_1:0": x_val}
+        input_names_with_port = ["input_1:0"]
+        output_names_with_port = ["output:0", "cell_state:0"]
+        self.run_test_case(feed_dict, input_names_with_port, output_names_with_port, 0.01)
 
 
 if __name__ == '__main__':
--- tf2onnx/rewriter/grublock_rewriter.py
+++ tf2onnx/rewriter/grublock_rewriter.py
@@ -39,10 +39,14 @@ class GRUBlockUnitRewriter(GRUUnitRewriter):
         return rnn_scope_name
 
     def get_weight_and_bias(self, match):
-        gate_kernel = get_weights_from_const_node(match.get_op("gate_kernel"))
-        gate_bias = get_weights_from_const_node(match.get_op("gate_bias"))
-        hidden_kernel = get_weights_from_const_node(match.get_op("hidden_kernel"))
-        hidden_bias = get_weights_from_const_node(match.get_op("hidden_bias"))
+
+        node = match.get_op("GRUBlockCell")
+        # from tf, it can be known that, the inputs index and meaning of input data is:
+        # 0-input, 1-state, 2-gate_kernel, 3-hidden_kernel, 4-gate_bias, 5-hidden_bias
+        gate_kernel = get_weights_from_const_node(node.inputs[2].inputs[0])
+        gate_bias = get_weights_from_const_node(node.inputs[4].inputs[0])
+        hidden_kernel = get_weights_from_const_node(node.inputs[3].inputs[0])
+        hidden_bias = get_weights_from_const_node(node.inputs[5].inputs[0])
         if not all([gate_kernel, gate_bias, hidden_kernel, hidden_bias]):
             log.error("rnn weights check failed, skip")
             sys.exit(-1)
@@ -56,184 +60,12 @@ class GRUBlockUnitRewriter(GRUUnitRewriter):
 
     @staticmethod
     def _state_switch_check(enter_target_node_input_id, identity_consumers, match):
-        concat_nodes = [c for c in identity_consumers if c == match.get_op("cell_inputs")]
-        if len(concat_nodes) == 1:
+        node = match.get_op("GRUBlockCell")
+        if node == identity_consumers[0]:
             log.debug("find state initializer value at " + enter_target_node_input_id)
             return enter_target_node_input_id
         else:
-            log.debug(str(len(concat_nodes)) + "Concat matching found, cannot identify state initializer")
             return None
-
-    def _connect_gru_state_to_graph(self, gru_node, exit_node, rnn_props):
-        # in tf, state output shape is: [batch, hidden]
-        # in onnx, output shape is: [number_directions, batch, hidden]
-        output_id = gru_node.output[1]
-        squeeze_node = make_onnx_node(self.g, "Squeeze", [output_id], attr={"axes": [0]})
-        gru_state_shape = self.g.get_shape(output_id)
-        self.g.set_shape(squeeze_node.output[0], [gru_state_shape[1], gru_state_shape[2]])
-        self.all_nodes.extend([squeeze_node])
-        self.g.replace_all_inputs(self.all_nodes, exit_node.output[0], squeeze_node.output[0])
-
-    def _output_switch_check(self, enter_target_node_input_id, identity_consumers, match):
-        ta_write_nodes = [c for c in identity_consumers if c.type == "TensorArrayWriteV3"]
-        if len(ta_write_nodes) == 1:
-            enter_target_node = self.g.get_node_by_name(enter_target_node_input_id)
-            if enter_target_node.type == "TensorArrayV3":
-                log.debug("found output switch node")
-                return enter_target_node_input_id
-            log.debug("found enter target node is not ta node")
-            return None
-        log.debug(str(len(ta_write_nodes)) + " TensorArrayWriteV3 matching found, cannot validate output switch")
-        return None
-
-    def _connect_gru_output_to_graph(self, gru_node, exit_node, rnn_props):
-        exit_consumers = self.g.find_output_consumers(exit_node.output[0])
-        gather_node = self._validate_output_exit_consumers(exit_consumers)
-        if len(exit_consumers) != 2 or not gather_node:
-            log.debug("gru output exit node has " + str(len(exit_consumers)) + " consumers")
-            raise ValueError("gru output exit node check failed")
-
-        # gather output for sure has shape [time, batch, hidden]
-        gather_output_id = gather_node.output[0]
-        log.debug("found output ta gather node " + gather_output_id)
-        # in tf batch major mode, output shape is : [batch, time, hidden]
-        # in time major mode, output shape is: [time, batch, hidden]
-        # in onnx, output shape is : [time, num_directions, batch, hidden]
-
-        output_id = gru_node.output[0]
-        squeeze_node = make_onnx_node(self.g, "Squeeze", [output_id], attr={"axes": [1]})
-        gru_output_shape = self.g.get_shape(output_id)
-        self.g.set_shape(squeeze_node.output[0], [gru_output_shape[0], gru_output_shape[2], gru_output_shape[3]])
-
-        if not rnn_props.time_major:
-            gather_consumers = self.g.find_output_consumers(gather_output_id)
-            gather_trans_consumers = [n for n in gather_consumers if check_is_timemajor_transpose(n)]
-            if len(gather_trans_consumers) != 1:
-                raise ValueError("batch major should expect a transpose after gather")
-            trans = gather_trans_consumers[0]  # trans has rnn scope name
-
-            # we just check the transpose here, but will not re-use it, because
-            # it may hold non-const perms. so we re-create a new transpose to replace it
-            attr = {"perm": np.array([1, 0, 2], dtype=np.int64)}
-            new_trans = make_onnx_node(self.g, "Transpose", [squeeze_node.output[0]], attr)
-            trans_input_shape = self.g.get_shape(squeeze_node.output[0])
-            self.g.replace_all_inputs(self.all_nodes, trans.output[0], new_trans.output[0])
-            self.g.set_shape(new_trans.output[0], [trans_input_shape[1], trans_input_shape[0], trans_input_shape[2]])
-            self.all_nodes.extend([new_trans])
-
-        self.g.replace_all_inputs(self.all_nodes, gather_output_id, squeeze_node.output[0])
-        self.all_nodes.extend([squeeze_node])
-
-    @staticmethod
-    def _validate_output_exit_consumers(exit_consumers):
-        if len(exit_consumers) != 2:
-            return None
-
-        gather_node = None
-        for n in exit_consumers:
-            if n.type == "TensorArrayGatherV3":
-                gather_node = n
-            elif n.type == "TensorArraySizeV3":
-                continue
-            else:
-                return None
-        return gather_node
-
-    def get_rnn_input_blacklist(self, rnn_weights, rnn_props):
-        var_init_nodes = []
-        for _, init_input_id in rnn_props.var_initializers.items():
-            init_node = self.g.get_node_by_name(init_input_id)
-            var_init_nodes.append(init_node)
-            self.must_keep_nodes.append(init_node)
-        blacklist_inputs = []
-        blacklist_inputs.extend(var_init_nodes)
-        # weight/bias inputs, and c/h initializer are dynamic_rnn/GRUCell's parameters.
-        # we will use them to filter out the dynamic_rnn's input tensor.
-        for _, value in rnn_weights.items():
-            blacklist_inputs.append(value.node)
-
-        return blacklist_inputs
-
-    def process_weights_and_bias(self, rnn_weights, rnn_props):
-        """
-        why split the data in this way should refer to code of tensorflow GRU cell and official document of ONNX GRU
-        """
-        # from code of tensorflow GRU cell, it can be known that shape of hidden_kernel(or candidate_kernel)
-        # is (input_size+hidden_unit, hidden_unit)
-        hidden_size = rnn_weights["hidden_kernel"].value.shape[1]
-        input_size = rnn_weights["hidden_kernel"].value.shape[0] - hidden_size
-        weight_dtype = rnn_weights["hidden_kernel"].dtype
-        bias_dtype = rnn_weights["hidden_bias"].dtype
-        # below code will use same notation as ONNX document
-        # z means update gate, r means reset gate, h means hidden gate;
-        # at this time weights of gate include input and state, will split it next
-        r_kernel, z_kernel = np.split(rnn_weights["gate_kernel"].value, [hidden_size], axis=1)
-        h_kernel = rnn_weights["hidden_kernel"].value
-        r_bias, z_bias = np.split(rnn_weights["gate_bias"].value, [hidden_size], axis=0)
-        h_bias = rnn_weights["hidden_bias"].value
-        # ONNX GRU split weights of input and state, so have to split *_kernel
-        input_r_kernel, state_r_kernel = np.split(r_kernel, [input_size], axis=0)
-        input_z_kernel, state_z_kernel = np.split(z_kernel, [input_size], axis=0)
-        input_h_kernel, state_h_kernel = np.split(h_kernel, [input_size], axis=0)
-        W_zrh = np.concatenate((input_z_kernel, input_r_kernel, input_h_kernel), axis=1)
-        R_zrh = np.concatenate((state_z_kernel, state_r_kernel, state_h_kernel), axis=1)
-        # transpose weight matrix
-        W_zrh = np.transpose(np.expand_dims(W_zrh, axis=0), axes=(0, 2, 1))
-        R_zrh = np.transpose(np.expand_dims(R_zrh, axis=0), axes=(0, 2, 1))
-        W_zrh = W_zrh.astype(weight_dtype)
-        R_zrh = R_zrh.astype(weight_dtype)
-        assert W_zrh.shape == (1, 3*hidden_size, input_size)
-        assert R_zrh.shape == (1, 3*hidden_size, hidden_size)
-        Wb_zrh = np.concatenate((z_bias, r_bias, h_bias), axis=0)
-        # tf don't have bias for state, so use 0 instead
-        zero = np.zeros_like(z_bias)
-        Rb_zrh = np.concatenate((zero, zero, zero), axis=0)
-        B_zrh = np.concatenate((Wb_zrh, Rb_zrh), axis=0)
-        B_zrh = np.expand_dims(B_zrh, axis=0)
-        B_zrh = B_zrh.astype(bias_dtype)
-        assert B_zrh.shape == (1, 6*hidden_size)
-        # create const ONNX node
-        w_name = utils.make_name("W")
-        w_node = self.g.make_const(w_name, W_zrh, skip_conversion=True)
-
-        r_name = utils.make_name("R")
-        r_node = self.g.make_const(r_name, R_zrh, skip_conversion=True)
-
-        b_name = utils.make_name("B")
-        b_node = self.g.make_const(b_name, B_zrh, skip_conversion=True)
-
-        rnn_props.input_size = input_size
-        rnn_props.hidden_size = hidden_size
-        rnn_props.onnx_input_ids["W"] = w_node.output[0]
-        rnn_props.onnx_input_ids["R"] = r_node.output[0]
-        rnn_props.onnx_input_ids["B"] = b_node.output[0]
-
-    def process_var_init_nodes(self, rnn_props):
-        assert "state" in rnn_props.var_initializers.keys()
-        found_node_id = rnn_props.var_initializers["state"]
-        init_state_id = self._process_init_nodes(found_node_id, rnn_props)
-        rnn_props.onnx_input_ids["initial_state"] = init_state_id
-
-    def _process_init_nodes(self, initializer_input_id, rnn_props):
-        # copy from  lstm_rewriter
-        # todo: remove this once Fill ops is supported
-        fill_ch_init_node = self._workaround_fill_ch_init_node(initializer_input_id, rnn_props)
-        if fill_ch_init_node:
-            return fill_ch_init_node.output[0]
-
-        node = self.g.get_node_by_name(initializer_input_id)
-        self.must_keep_nodes.append(node)
-        if node.is_const():
-            val = node.get_tensor_value()
-            initial_name = utils.make_name("Const")
-            new_val = np.expand_dims(val, axis=0)
-            const_node = self.g.make_const(initial_name, new_val)
-            return const_node.output[0]
-        else:
-            squeeze_node = make_onnx_node(self.g, "Unsqueeze", [initializer_input_id], attr={"axes": [0]})
-            self.g.replace_all_inputs(self.g.get_nodes(), initializer_input_id, squeeze_node.output[0])
-            self.all_nodes.append(squeeze_node)
-            return squeeze_node.output[0]
 
     @staticmethod
     def get_rnn_activation(match):
